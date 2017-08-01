8a6f77b3b2781b44e60c49fe80710313274e9e96
Monique Williamson <monique@catalyst-eu.net>
2017-08-01 17:50:32 +0100

    probably broke things trying to make login work

--- quoteriser/urls.py
+++ quoteriser/urls.py
@@ -19,4 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^quotes/', include('quotes.urls', namespace="quotes")),
+    url('^', include('django.contrib.auth.urls'))
+#Add Django site authentication urls (for login, logout, password management)
+
 ]
